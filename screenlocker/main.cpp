8e922e41c3f460e59112337045b638d99785c0b4
reionwong <reionwong@gmail.com>
2021-07-27 23:02:51 +0800

    Add translations

--- screenlocker/main.cpp
+++ screenlocker/main.cpp
@@ -1,8 +1,23 @@
 #include "application.h"
+#include <QTranslator>
+#include <QLocale>
 
 int main(int argc, char *argv[])
 {
     Application app(argc, argv);
+
+    // Translations
+    QLocale locale;
+    QString qmFilePath = QString("%1/%2.qm").arg("/usr/share/cutefish-screenlocker/translations/").arg(locale.name());
+    if (QFile::exists(qmFilePath)) {
+        QTranslator *translator = new QTranslator(app.instance());
+        if (translator->load(qmFilePath)) {
+            app.installTranslator(translator);
+        } else {
+            translator->deleteLater();
+        }
+    }
+
     app.setQuitOnLastWindowClosed(false);
     app.initialViewSetup();
     return app.exec();
