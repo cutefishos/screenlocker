0a90cf8e856bd12c9426740e3016f0426311ac0b
Halit Sever <dunyayuvarlakmuratbabandir@gmail.com>
2021-08-03 23:17:43 +0000

    Translated using Weblate (Turkish)
    Currently translated at 100.0% (4 of 4 strings)
    
    Translation: CutefishOS/ScreenLocker
    Translate-URL: https://hosted.weblate.org/projects/cutefishos/screenlocker/tr/

--- translations/tr_TR.ts
+++ translations/tr_TR.ts
@@ -6,22 +6,22 @@
     <message>
         <location filename="../screenlocker/qml/LockScreen.qml" line="202"/>
         <source>Password</source>
-        <translation type="unfinished"></translation>
+        <translation>Şifre</translation>
     </message>
     <message>
         <location filename="../screenlocker/qml/LockScreen.qml" line="225"/>
         <source>Unlock</source>
-        <translation type="unfinished"></translation>
+        <translation>Kilidi kaldır</translation>
     </message>
     <message>
         <location filename="../screenlocker/qml/LockScreen.qml" line="274"/>
         <source>Please enter your password</source>
-        <translation type="unfinished"></translation>
+        <translation>Lütfen şifrenizi girin</translation>
     </message>
     <message>
         <location filename="../screenlocker/qml/LockScreen.qml" line="284"/>
         <source>Unlocking failed</source>
-        <translation type="unfinished"></translation>
+        <translation>Giriş başarısız oldu</translation>
     </message>
 </context>
 </TS>
