8f1db8c32f6b14cee359648dcb0825fef07b7594
Theekshana Fernando <rtheekshanafernando@gmail.com>
2021-08-09 16:15:13 +0000

    Translated using Weblate (Sinhala)
    Currently translated at 100.0% (4 of 4 strings)
    
    Translation: CutefishOS/ScreenLocker
    Translate-URL: https://hosted.weblate.org/projects/cutefishos/screenlocker/si/

--- translations/si_LK.ts
+++ translations/si_LK.ts
@@ -6,22 +6,22 @@
     <message>
         <location filename="../screenlocker/qml/LockScreen.qml" line="202"/>
         <source>Password</source>
-        <translation type="unfinished"></translation>
+        <translation>මුර පදය</translation>
     </message>
     <message>
         <location filename="../screenlocker/qml/LockScreen.qml" line="225"/>
         <source>Unlock</source>
-        <translation type="unfinished"></translation>
+        <translation>අගුල අරින්න</translation>
     </message>
     <message>
         <location filename="../screenlocker/qml/LockScreen.qml" line="274"/>
         <source>Please enter your password</source>
-        <translation type="unfinished"></translation>
+        <translation>කරුණාකර ඔබගේ මුරපදය අතුලත් කරන්න</translation>
     </message>
     <message>
         <location filename="../screenlocker/qml/LockScreen.qml" line="284"/>
         <source>Unlocking failed</source>
-        <translation type="unfinished"></translation>
+        <translation>අගුල ඇරීම අසාර්ථක විය</translation>
     </message>
 </context>
 </TS>
