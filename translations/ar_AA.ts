0cbdd98e611e41f749dcdf68cfb5f4b53f537941
Muadh Abdulaziz <m_abdulaziz@tutamail.com>
2021-08-13 11:06:29 +0000

    Translated using Weblate (Arabic)
    Currently translated at 100.0% (4 of 4 strings)
    
    Translation: CutefishOS/ScreenLocker
    Translate-URL: https://hosted.weblate.org/projects/cutefishos/screenlocker/ar/

--- translations/ar_AA.ts
+++ translations/ar_AA.ts
@@ -6,22 +6,22 @@
     <message>
         <location filename="../screenlocker/qml/LockScreen.qml" line="202"/>
         <source>Password</source>
-        <translation type="unfinished"></translation>
+        <translation>كلمة المرور</translation>
     </message>
     <message>
         <location filename="../screenlocker/qml/LockScreen.qml" line="225"/>
         <source>Unlock</source>
-        <translation type="unfinished"></translation>
+        <translation>إلغاء القُفْل</translation>
     </message>
     <message>
         <location filename="../screenlocker/qml/LockScreen.qml" line="274"/>
         <source>Please enter your password</source>
-        <translation type="unfinished"></translation>
+        <translation>يُرجى إدخال كلمة مرورك</translation>
     </message>
     <message>
         <location filename="../screenlocker/qml/LockScreen.qml" line="284"/>
         <source>Unlocking failed</source>
-        <translation type="unfinished"></translation>
+        <translation>فَشِل إلغاء القُفْل</translation>
     </message>
 </context>
 </TS>
