6f7eb27c4089b0d5e30a851252522e822b14c7b4
circonfl3x <sayalelowesi456@gmail.com>
2021-08-17 12:04:23 +0000

    Translated using Weblate (Swahili)
    Currently translated at 75.0% (3 of 4 strings)
    
    Translation: CutefishOS/ScreenLocker
    Translate-URL: https://hosted.weblate.org/projects/cutefishos/screenlocker/sw/

--- translations/sw.ts
+++ translations/sw.ts
@@ -6,22 +6,22 @@
     <message>
         <location filename="../screenlocker/qml/LockScreen.qml" line="202"/>
         <source>Password</source>
-        <translation type="unfinished"></translation>
+        <translation>Neno la siri</translation>
     </message>
     <message>
         <location filename="../screenlocker/qml/LockScreen.qml" line="225"/>
         <source>Unlock</source>
-        <translation type="unfinished"></translation>
+        <translation>Fungua</translation>
     </message>
     <message>
         <location filename="../screenlocker/qml/LockScreen.qml" line="274"/>
         <source>Please enter your password</source>
-        <translation type="unfinished"></translation>
+        <translation>Tafadhali ingiza neno la siri</translation>
     </message>
     <message>
         <location filename="../screenlocker/qml/LockScreen.qml" line="284"/>
         <source>Unlocking failed</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">Kufunguliwa limeshindwa</translation>
     </message>
 </context>
 </TS>
