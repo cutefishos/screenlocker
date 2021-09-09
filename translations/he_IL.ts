a3fa57b5f7ccbaa3c42a7320ad932149853fe993
bugthug <avivyodfat@protonmail.com>
2021-09-09 07:39:35 +0000

    Translated using Weblate (Hebrew (Israel))
    Currently translated at 75.0% (3 of 4 strings)
    
    Translation: CutefishOS/ScreenLocker
    Translate-URL: https://hosted.weblate.org/projects/cutefishos/screenlocker/he_IL/

--- translations/he_IL.ts
+++ translations/he_IL.ts
@@ -6,22 +6,22 @@
     <message>
         <location filename="../screenlocker/qml/LockScreen.qml" line="202"/>
         <source>Password</source>
-        <translation type="unfinished"></translation>
+        <translation>סיסמא</translation>
     </message>
     <message>
         <location filename="../screenlocker/qml/LockScreen.qml" line="225"/>
         <source>Unlock</source>
-        <translation type="unfinished"></translation>
+        <translation>בטל נעילה</translation>
     </message>
     <message>
         <location filename="../screenlocker/qml/LockScreen.qml" line="274"/>
         <source>Please enter your password</source>
-        <translation type="unfinished"></translation>
+        <translation type="unfinished">הזן את הסיסמא שלך</translation>
     </message>
     <message>
         <location filename="../screenlocker/qml/LockScreen.qml" line="284"/>
         <source>Unlocking failed</source>
-        <translation type="unfinished"></translation>
+        <translation>ההתחברות נכשלה</translation>
     </message>
 </context>
 </TS>
