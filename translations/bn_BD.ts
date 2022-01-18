e5a78f4b2362f14503401a453c087e673c150331
Mohammad Shoriful Islam Ronju <smronju@gmail.com>
2022-01-18 18:35:28 +0000

    Translated using Weblate (Bengali (Bangladesh))
    Currently translated at 100.0% (4 of 4 strings)
    
    Translation: CutefishOS/ScreenLocker
    Translate-URL: https://hosted.weblate.org/projects/cutefishos/screenlocker/bn_BD/

--- translations/bn_BD.ts
+++ translations/bn_BD.ts
@@ -6,22 +6,22 @@
     <message>
         <location filename="../screenlocker/qml/LockScreen.qml" line="202"/>
         <source>Password</source>
-        <translation type="unfinished"></translation>
+        <translation>পাসওয়ার্ড</translation>
     </message>
     <message>
         <location filename="../screenlocker/qml/LockScreen.qml" line="225"/>
         <source>Unlock</source>
-        <translation type="unfinished"></translation>
+        <translation>আনলক করুন</translation>
     </message>
     <message>
         <location filename="../screenlocker/qml/LockScreen.qml" line="274"/>
         <source>Please enter your password</source>
-        <translation type="unfinished"></translation>
+        <translation>আপনার পাসওয়ার্ড দিন</translation>
     </message>
     <message>
         <location filename="../screenlocker/qml/LockScreen.qml" line="284"/>
         <source>Unlocking failed</source>
-        <translation type="unfinished"></translation>
+        <translation>আনলক করা ব্যর্থ হয়েছে</translation>
     </message>
 </context>
 </TS>
