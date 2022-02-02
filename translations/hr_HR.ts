f2a297e2f67abaae2a0e58942b640f6d4432da09
Albion Haxhijaj <albion.haxhijaj@skole.hr>
2022-02-02 21:51:21 +0000

    Translated using Weblate (Croatian)
    Currently translated at 100.0% (4 of 4 strings)
    
    Translation: CutefishOS/ScreenLocker
    Translate-URL: https://hosted.weblate.org/projects/cutefishos/screenlocker/hr/

--- translations/hr_HR.ts
+++ translations/hr_HR.ts
@@ -6,22 +6,22 @@
     <message>
         <location filename="../screenlocker/qml/LockScreen.qml" line="202"/>
         <source>Password</source>
-        <translation type="unfinished"></translation>
+        <translation>Lozinka</translation>
     </message>
     <message>
         <location filename="../screenlocker/qml/LockScreen.qml" line="225"/>
         <source>Unlock</source>
-        <translation type="unfinished"></translation>
+        <translation>Otključavanje</translation>
     </message>
     <message>
         <location filename="../screenlocker/qml/LockScreen.qml" line="274"/>
         <source>Please enter your password</source>
-        <translation type="unfinished"></translation>
+        <translation>Upiši lozinku</translation>
     </message>
     <message>
         <location filename="../screenlocker/qml/LockScreen.qml" line="284"/>
         <source>Unlocking failed</source>
-        <translation type="unfinished"></translation>
+        <translation>Otključavanje neuspješno</translation>
     </message>
 </context>
 </TS>
